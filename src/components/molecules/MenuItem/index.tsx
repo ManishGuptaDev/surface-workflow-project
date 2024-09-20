6ae6e2feb0d6116341a56de1c5c91e076a94bb37
Manish Gupta <manish.gta1@gmail.com>
2024-09-20 19:37:18 +0530

    polishing

--- src/components/molecules/MenuItem/index.tsx
+++ src/components/molecules/MenuItem/index.tsx
@@ -11,8 +11,8 @@ const MenuItem: React.FC<MenuItemProps> = ({ name, path, icon }) => {
   return (
     <Link href={path}>
       <div className="flex items-center py-2 rounded-lg hover:bg-gray-200 transition duration-200 gap-4">
-        <span className="h-5 w-5">{icon}</span>
-        <span className="text-gray-800 text-[16px] font-medium">{name}</span>
+        <span className="text-[#383F5080]">{icon}</span>
+        <span className="text-[#383F5080] text-[16px] font-medium">{name}</span>
       </div>
     </Link>
   );
--- src/lib/consts/menuItems.tsx
+++ src/lib/consts/menuItems.tsx
@@ -1,39 +1,47 @@
-import { Home, Funnel, Lead, Segment, Workflow, Integration, Setting } from "~/assets/icons";
+import {
+  Home,
+  Funnel,
+  Lead,
+  Segment,
+  Workflow,
+  Integration,
+  Setting,
+} from "~/assets/icons";
 
 export const menuItems = [
-    {
-        name: "Overview",
-        path: "/overview",
-        icon: <Home />,
-      },
-      {
-        name: "Funnels",
-        path: "/funnels",
-        icon: <Funnel />,
-      },
-      {
-        name: "Leads",
-        path: "/leads",
-        icon: <Lead />,
-      },
-      {
-        name: "Segments",
-        path: "/segments",
-        icon: <Segment />,
-      },
-      {
-        name: "Workflows",
-        path: "/workflows",
-        icon: <Workflow />,
-      },
-      {
-        name: "Integrations",
-        path: "/integrations",
-        icon: <Integration />,
-      },
-      {
-        name: "Settings",
-        path: "/settings",
-        icon: <Setting />,
-      },    
-]
+  {
+    name: "Overview",
+    path: "/overview",
+    icon: <Home className="h-5 w-5" />,
+  },
+  {
+    name: "Funnels",
+    path: "/funnels",
+    icon: <Funnel className="h-5 w-5" />,
+  },
+  {
+    name: "Leads",
+    path: "/leads",
+    icon: <Lead className="h-5 w-5" />,
+  },
+  {
+    name: "Segments",
+    path: "/segments",
+    icon: <Segment className="h-5 w-5" />,
+  },
+  {
+    name: "Workflows",
+    path: "/workflows",
+    icon: <Workflow className="h-5 w-5" />,
+  },
+  {
+    name: "Integrations",
+    path: "/integrations",
+    icon: <Integration className="h-5 w-5" />,
+  },
+  {
+    name: "Settings",
+    path: "/settings",
+    icon: <Setting className="h-5 w-5" />,
+  },
+];
